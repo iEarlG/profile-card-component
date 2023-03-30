72d1bc1ab25eefa5e319e13e7d794a819acb2da5
Earl A. Villapaz <jasonburning16@gmail.com>
2023-03-30 21:58:18 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Contents from './components/Contents';
+
+const App = () => {
+  return (
+    <div>
+      <Contents />
+
+      <div className="attribution">
+        Challenge by<a href="https://www.frontendmentor.io?ref=challenge" target="_blank"> Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/iEarlG" target="_blank">Earl A. Villapaz</a>.
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Contents.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Contents = () => {
+  return (
+    <div>
+        Victor Crest
+  26
+  London
+
+  80K
+  Followers
+
+  803K
+  Likes
+
+  1.4K
+  Photos
+    </div>
+  );
+};
+
+export default Contents;
