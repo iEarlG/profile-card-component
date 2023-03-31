994a18b57f71991861aa19240ec7ded4941e67f8
Earl A. Villapaz <jasonburning16@gmail.com>
2023-03-31 16:48:24 +0800

    second commit

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -1,21 +1,44 @@
 import React from 'react';
 
+import coverPhoto from '../assets/images/bg-pattern-card.svg';
+import profilePhoto from '../assets/images/image-victor.jpg';
+
 const Contents = () => {
   return (
-    <div>
-        Victor Crest
-  26
-  London
+    <main>
+      <div className="container">
+        <div className="cover__photo-box">
+          <img src={coverPhoto} alt="coverPhoto" className="cover-img" />
+        </div>
 
-  80K
-  Followers
+        <div className="profile-photo">
+          <img src={profilePhoto} alt="profilePhoto" className="profile-img" />
+        </div>
 
-  803K
-  Likes
+        <div className="profile-info">
+          <h1>Victor Crest</h1>
+          <p>26</p>
+        </div>
+        <p className="profile-location">London</p>
 
-  1.4K
-  Photos
-    </div>
+        <div></div>
+
+        <div className="profile__stats">
+          <div className="followers">
+            <h1>80k</h1>
+            <p>Followers</p>
+          </div>
+          <div className="likes">
+            <h1>803k</h1>
+            <p>Likes</p>
+          </div>
+          <div className="photos">
+            <h1>1.4k</h1>
+            <p>Photos</p>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
